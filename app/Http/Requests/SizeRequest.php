4631383319aac2e0ec1d6aed4b634fb83ac21d6f
_Phi <buingocphinn@gmail.com>
2024-06-12 10:40:14 +0700

    feat: validate size

---
+++ app/Http/Requests/SizeRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+
+class SizeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = ApiResponse(false,Response::HTTP_BAD_REQUEST,$validator->errors(),null);
+        throw (new ValidationException($validator,$response));
+    }
+}
