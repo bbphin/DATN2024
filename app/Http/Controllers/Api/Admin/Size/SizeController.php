cfc715faa4d922d299a0685aff66fc09411c57eb
_Phi <buingocphinn@gmail.com>
2024-06-12 11:44:21 +0700

    feat: CRUD size

--- app/Http/Controllers/Api/Admin/Size/SizeController.php
+++ app/Http/Controllers/Api/Admin/Size/SizeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Api\Admin\Size;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SizeRequest;
 use App\Http\Resources\SizeResource;
 use App\Models\Size;
+use Cloudinary\Api\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Spatie\FlareClient\Api;
 
 class SizeController extends Controller
 {
@@ -15,48 +18,42 @@ class SizeController extends Controller
      */
     public function index()
     {
-        $data = Size::all();
-        return ApiResponse(true, Response::HTTP_OK, messageResponseData(), SizeResource::collection($data));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        try {
+            $data = Size::paginate(5);
+            return ApiResponse(true, Response::HTTP_OK, messageResponseData(), SizeResource::collection($data));
+        } catch (\Exception $e) {
+            return ApiResponse(false, Response::HTTP_BAD_REQUEST, $e->getMessage(), null);
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(SizeRequest $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        try {
+            $data = Size::create($request->all());
+            return ApiResponse(true, Response::HTTP_CREATED, messageResponseActionSuccess(), new SizeResource($data));
+        } catch (\Exception $e) {
+            return ApiResponse(false, Response::HTTP_BAD_REQUEST, $e->getMessage(), null);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(SizeRequest $request, string $id)
     {
-        //
+        try {
+            $size = Size::find($id);
+            if (empty($size)) {
+                return ApiResponse(false, Response::HTTP_BAD_REQUEST, messageResponseNotFound(), null);
+            }
+            $size->update($request->all());
+            return ApiResponse(true, Response::HTTP_OK, messageResponseActionSuccess(), new SizeResource($size));
+        } catch (\Exception $e) {
+            return ApiResponse(true, Response::HTTP_BAD_REQUEST, $e->getMessage(), null);
+        }
     }
 
     /**
@@ -64,6 +61,15 @@ class SizeController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $size = Size::find($id);
+            if (empty($size)) {
+                return ApiResponse(false, Response::HTTP_BAD_REQUEST, messageResponseNotFound(), null);
+            }
+            $size->delete();
+            return ApiResponse(true, Response::HTTP_OK, messageResponseActionSuccess(), new SizeResource($size));
+        } catch (\Exception $e) {
+            return ApiResponse(false, Response::HTTP_BAD_REQUEST, $e->getMessage(), null);
+        }
     }
 }
