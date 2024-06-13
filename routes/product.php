7f53ea188d68df68bd412c00843ad53a62bc27fb
_Phi <buingocphinn@gmail.com>
2024-06-13 23:41:14 +0700

    feat: route get product by product_category

--- routes/product.php
+++ routes/product.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Admin\Product\ProductController;
+use App\Http\Controllers\Api\Client\Product\ProductController as ClientProductController;
 
 Route::apiResource('products',ProductController::class);
 
@@ -13,3 +14,7 @@ Route::controller(ProductController::class)->name('products.')->group(function (
     Route::get('products/restore/{id}','restore')->name('restore');
 });
 
+// hien thi san pham theo product category
+Route::controller(ClientProductController::class)->name('product.')->group(function () {
+    Route::get('product-category/product/{id}','showProductsByproductCategory')->name('product_category');
+});
