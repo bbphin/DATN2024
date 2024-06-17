3fa029b234ebe6cac6b7bbf49c8ab9ab8b89a629
_Phi <buingocphinn@gmail.com>
2024-06-17 09:45:16 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,10 @@ require_once __DIR__ . '/product_category.php';
 require_once __DIR__ . '/product.php';
 // Coupon
 require_once __DIR__ . '/coupon.php';
-
+// Wishlist
+require_once __DIR__ . '/wishlist.php';
+// Review
+require_once __DIR__ . '/review.php';
 
 
 
