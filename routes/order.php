657f406330976bea5eae0a59203528b8d37325c4
_Phi <buingocphinn@gmail.com>
2024-06-23 13:44:36 +0700

    feat: route get order for user

--- routes/order.php
+++ routes/order.php
@@ -6,5 +6,6 @@ use App\Http\Controllers\Api\Client\Order\OrderController;
 
 
 Route::controller(OrderController::class)->name('order.')->group(function() {
+    Route::get('orders','index')->name('index');
     Route::post('order','checkout')->name('checkout');
 });
